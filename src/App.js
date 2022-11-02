f8b61de1e9666225a5a68ae5ade7fc078e5f0d4c
Maria Clara Sanchez Villegas <msanch60@eafit.edu.co>
2022-11-02 18:29:34 -0500

    Añadiendo Merengon msanh60

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ const rows = [
   createData('Torta', 200, 16.0, 49, 3.9),
   createData('Brazo de Reina', 400, 16.0, 49, 3.9),
   createData('Brownie', 123, 16.0, 49, 3.9),
+  createData('Merengon', 1000, 20.0, 100, 0.1),
 ];
 
 export default function BasicTable() {
