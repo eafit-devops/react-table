817766e8cbbf54d0a1768d85e8b56351d5b3699b
Jorge  I Ramirez M <jorge.ramirez@himedsolutions.com>
2022-11-02 18:55:59 -0500

    Creación de Tiramisu

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ const rows = [
   createData('Torta', 200, 16.0, 49, 3.9),
   createData('Brazo de Reina', 400, 16.0, 49, 3.9),
   createData('Brownie', 123, 16.0, 49, 3.9),
+  createData('Tiramisu', 1307, 16.0, 49, 3.9),
 ];
 
 export default function BasicTable() {
