98545b65e01e2be460936004a4f132ba243647d7
Mauricio Restrepo A <mrestrepoagu@gmail.com>
2022-11-02 19:24:01 -0500

    Se corrige error

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ const rows = [
   createData('Torta', 200, 16.0, 49, 3.9),
   createData('Brazo de Reina', 400, 16.0, 49, 3.9),
   createData('Brownie', 123, 16.0, 49, 3.9),
-  createData('Basbousa', 123, 16.0, 49, 3.9)
-  createData('IslasFlotantes', 150, 15.0, 50, 3.9),
+  createData('Basbousa', 123, 16.0, 49, 3.9),
+  createData('IslasFlotantes', 150, 15.0, 50, 3.9)
 ];
 
 export default function BasicTable() {
