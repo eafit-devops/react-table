efc3a01640f9116bd766109bcdb283c32264074f
Daniel Ramírez <nassao@gmail.com>
2022-11-09 01:04:14 -0500

    Base para testing

--- src/App.js
+++ src/App.js
@@ -32,34 +32,39 @@ const rows = [
 
 export default function BasicTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    <>
+      <h1 id="titulo-tabla" data-testid="h1-titulo-tabla">
+        Tabla de Postres
+      </h1>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Se renderiza la App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  expect(screen).toBeDefined();
+});
+
+test('Se renderiza el título', () => {
+  render(<App />);
+  const linkElement = screen.getByText("Tabla de Postres");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('El título tiene ID', () => {
+  render(<App />);
+  // eslint-disable-next-line testing-library/no-node-access
+  const linkElement = document.querySelector("#titulo-tabla");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('El título tiene TestID', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId("h1-titulo-tabla");
   expect(linkElement).toBeInTheDocument();
 });
