d9148450fedccb948a21d77dfef6644a527ffd8d
Marlon Gomez <marlonsgy@gmail.com>
2022-11-17 00:28:25 +0000

    tiny refactor

--- src/BadCode.js
+++ src/BadCode.js
@@ -1,31 +1,37 @@
 import React from 'react'
 
 export default function BadCode() {
- const function calcularCosito(x, y, z) {
-	// Toma el medio y lo multiplica para uqe retorne 4 veces su valor
-	var i=4*y;
-	// Suma el medio con los extremos alto y bajo para tener la suma de todos los valores a sumar
-	var j= x+i+y;
-	//Dividir entre 6
-	var k = j/6
-	return k;
-	};
-
-	const error = (X,Y) =>{
-					return Math.sqrt(Math.pow((X-B), 2)/36);
-	};
-
-	const analProb = (esperado, alto, bajo) => {
-		const prob = calcularCosito(alto, esperado, bajo);
-		const errorARetornarEnLaFuncion= error(alto, bajo);
-		return {prob, e}
+	// TODO: confirm names for domain
+	const testValues = {
+		expected: 50,
+		high: 70,
+		low: 45,
 	}
 
-	const render = analProb(50, 70, 45);
+	const getProbability = (expected, high) => {
+		return (expected + (5 * high)) / 6;
+	};
+
+	const getErrorRate = (high, low) => {
+		return Math.sqrt(Math.pow((high-low), 2) / 36);
+	};
+
+	const getProbabilityAndErrorRate = (expected, high, low) => {
+		const prob = getProbability(expected, high, low);
+		const err = getErrorRate(high, low);
+		return {prob, err}
+	}
+
+	const render = getProbabilityAndErrorRate(
+		testValues.expected,
+		testValues.high,
+		testValues.low,
+	);
+
 	return (
 		<div>
 			<p>El esperado es {render.prob}</p>
-			<p>El error es {render.e}</p>
+			<p>El error es {render.err}</p>
 		</div>
 	)
 }
